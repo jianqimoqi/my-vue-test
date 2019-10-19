dd835b23bf97e1b252f88d0d8c62dc3f38b6f1fd
jianqimoqi <wjbxndx@163.com>
2019-10-19 09:50:08 +0800

    修改Home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,45 +1,22 @@
 <!--首页-->
 <template>
   <div class="home">
-    <el-menu
-      :default-active="activeIndex"
-      class="el-menu-demo"
-      mode="horizontal"
-      @select="handleSelect"
-      background-color="#545c64"
-      text-color="#fff"
-      active-text-color="#ffd04b"
-      router>
-      <!-- router: 在导航栏中以index作为path进行路由跳转 -->
-      <el-menu-item index="/home/buyCar">购物车</el-menu-item>
-      <el-submenu index="2">
-        <template slot="title">我的工作台</template>
-        <el-menu-item index="/home/about">about</el-menu-item>
-        <el-menu-item index="2-2">选项2</el-menu-item>
-        <el-menu-item index="2-3">选项3</el-menu-item>
-        <el-submenu index="2-4">
-          <template slot="title">选项4</template>
-          <el-menu-item index="2-4-1">选项1</el-menu-item>
-          <el-menu-item index="2-4-2">选项2</el-menu-item>
-          <el-menu-item index="2-4-3">选项3</el-menu-item>
-        </el-submenu>
-      </el-submenu>
-      <el-menu-item index="3" disabled>消息中心</el-menu-item>
-      <el-menu-item index="4"><a href="https://www.ele.me" target="_blank">订单管理</a></el-menu-item>
-    </el-menu>
+    <Nav :NavActiveIndex="activeIndex"></Nav>
     <router-view/>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
+import Nav from './Nav.vue'
 
 export default {
   name: 'home',
-  components: { },
+  components: {
+    Nav
+  },
   data () {
     return {
-      activeIndex: '1'
+      activeIndex: 'aaa'
     }
   },
   methods: {
