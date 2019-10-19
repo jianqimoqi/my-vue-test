5640c9710f07128f3d11a188566942c253b0bd99
jianqimoqi <wjbxndx@163.com>
2019-10-19 09:49:35 +0800

    修改BuyCar

--- src/components/BuyCar.vue
+++ src/components/BuyCar.vue
@@ -11,25 +11,34 @@
         <el-col :span="3">操作</el-col>
       </div>
     </div>
-    <div class="hasCloth">
+    <div class="hasCloth" v-if="list.length">
       <ul class="clothBox">
-        <li class="cloth-tr">
-          <el-col :span="6">商品信息</el-col>
-          <el-col :span="3">尺码/颜色</el-col>
-          <el-col :span="3">吊牌价</el-col>
-          <el-col :span="3">数量</el-col>
-          <el-col :span="3">折扣</el-col>
-          <el-col :span="3">总金额</el-col>
+        <li v-for="(item, index) in list" :key="index" class="cloth-tr">
+          <el-col :span="6">
+            <el-checkbox v-model="selectArrCloth" :label="item.id" @change="item.checked=! item.checked">{{item.name}}</el-checkbox>
+          </el-col>
+          <el-col :span="3">{{item.size}}/{{item.color}}</el-col>
+          <el-col :span="3">{{item.price}}</el-col>
+          <el-col :span="3" class="ocloth-count">
+            <span @click=decrease(item)>-</span>
+            {{item.count}}
+            <span @click=increase(item)>+</span>
+          </el-col>
+          <el-col :span="3">{{item.active}}</el-col>
+          <el-col :span="3">{{item.active*item.price*item.count}}</el-col>
           <el-col :span="3">
-            <img class="delCloth" src="@/assets/images/delIcon.png" title="删除">
+            <img class="delCloth" src="@/assets/images/delIcon.png" title="删除" @click="del(item)">
           </el-col>
         </li>
       </ul>
     </div>
-    <div class="totolCount">
-      <div class="checkAll">全选</div>
-      <div class="totalPrice">合计</div>
-      <div class="clothCount">已选商品</div>
+    <div v-else>购物车没有商品，快去逛逛吧</div>
+    <div class="totalCount">
+      <div class="checkAll">
+        <el-checkbox v-model="checkAll" @change="getAllCheck">全选</el-checkbox>
+      </div>
+      <div class="totalPrice">合计{{totalPrice}}元</div>
+      <div class="clothCount">已选商品{{totalCount}}件</div>
     </div>
   </div>
 </template>
@@ -38,13 +47,123 @@
 export default {
   data () {
     return {
+      checkAll: false,
+      selectArrCloth: [],
+      totalCount: 0,
+      totalPrice: 0,
+      list: []
+    }
+  },
+  created () {
+    this.getList()
+  },
+  methods: {
+    getAllCheck (value) {
+      let selectArrCloth = []
+      if (value) {
+        this.list.forEach(item => {
+          item.checked = true
+          selectArrCloth.push(item.id)
+        })
+        this.selectArrCloth = selectArrCloth
+      } else {
+        this.list.forEach(item => {
+          item.checked = false
+        })
+        this.selectArrCloth = []
+      }
+      this.getTotalCount()
+    },
+    getTotalCount () {
+      let totalPrice = 0
+      let totalCount = 0
+      this.list.forEach(item => {
+        if (item.checked) {
+          totalPrice = totalPrice + (parseFloat(item.price).toFixed(2) * parseFloat(item.active) * item.count)
+          totalCount = totalCount + parseInt(item.count)
+        }
+      })
+      this.totalCount = totalCount
+      this.totalPrice = totalPrice
+    },
+    getList () {
+      let arr = [
+        {
+          name: '款式1',
+          count: '2',
+          size: 's',
+          color: '红色',
+          price: '100',
+          active: '0.5',
+          id: '1'
+        },
+        {
+          name: '款式2',
+          count: '2',
+          size: 's',
+          color: '红色',
+          price: '100',
+          active: '0.5',
+          id: '2'
+        },
+        {
+          name: '款式3',
+          count: '2',
+          size: 's',
+          color: '红色',
+          price: '100',
+          active: '0.5',
+          id: '3'
+        }
+      ]
+      arr.forEach(item => {
+        item.checked = false
+      })
+      this.list = arr
+    },
+    decrease (value) {
+      if (value.count === 0) return
+      value.count--
+      this.getTotalCount()
+    },
+    increase (value) {
+      value.count++
+      this.getTotalCount()
+    },
+    del (value) {
+      let index = this.list.findIndex(item => {
+        return item.id === value.id
+      })
+      this.list.splice(index, 1)
+      this.getTotalCount()
+    }
+  },
+  watch: {
+    selectArrCloth () {
+      this.getTotalCount()
+      if (this.selectArrCloth.length === this.list.length) {
+        this.checkAll = true
+      } else {
+        this.checkAll = false
+      }
     }
   }
 }
 </script>
 
 <style scoped>
-div {
-  background-color: #f5de19
+.haveCloth {
+  position: relative;
+  padding-top: 36px;
+}
+.noCloth {
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  margin-top: -50px;
+  margin-left: -200px;
+}
+.noCloth img {
+  width: 100px;
 }
 </style>
